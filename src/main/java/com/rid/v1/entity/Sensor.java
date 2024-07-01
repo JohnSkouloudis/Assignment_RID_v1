31ef572082b8d4ceeac9a15ddadf9fd8b6865505
iskoulou <johnskoul56@gmail.com>
2024-07-01 16:21:32 +0300

    created entities

--- src/main/java/com/rid/v1/entity/Sensor.java
+++ src/main/java/com/rid/v1/entity/Sensor.java
@@ -2,6 +2,9 @@ package com.rid.v1.entity;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "sensors")
 public class Sensor {
@@ -11,5 +14,73 @@ public class Sensor {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    @Column
+    private String type;
+    @Column
+    private String vendorName;
+    @Column
+    private String vendorEmail;
+    @Column
+    private String description;
+    @Column
+    private String location;
 
+    @OneToMany(mappedBy = "sensor",cascade = CascadeType.ALL)
+    private List<SensorReading> Readings=new ArrayList<>();
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getVendorName() {
+        return vendorName;
+    }
+
+    public void setVendorName(String vendorName) {
+        this.vendorName = vendorName;
+    }
+
+    public String getVendorEmail() {
+        return vendorEmail;
+    }
+
+    public void setVendorEmail(String vendorEmail) {
+        this.vendorEmail = vendorEmail;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public List<SensorReading> getReadings() {
+        return Readings;
+    }
+
+    public void setReadings(List<SensorReading> readings) {
+        Readings = readings;
+    }
 }
---
+++ src/main/java/com/rid/v1/entity/SensorReading.java
@@ -0,0 +1,89 @@
+package com.rid.v1.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table (name="readings")
+public class SensorReading {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private String readingType;
+    @Column
+    private String readingValue;
+    @Column
+    private String readingDate;
+    @Column
+    private String description;
+    @Column
+    private String time;
+
+    @ManyToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinColumn(name="sensor_id")
+    private Sensor sensor;
+
+
+    public SensorReading() {
+
+    }
+
+    public Sensor getSensor() {
+        return sensor;
+    }
+
+    public void setSensor(Sensor sensor) {
+        this.sensor = sensor;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getReadingType() {
+        return readingType;
+    }
+
+    public void setReadingType(String readingType) {
+        this.readingType = readingType;
+    }
+
+    public String getReadingValue() {
+        return readingValue;
+    }
+
+    public void setReadingValue(String readingValue) {
+        this.readingValue = readingValue;
+    }
+
+    public String getReadingDate() {
+        return readingDate;
+    }
+
+    public void setReadingDate(String readingDate) {
+        this.readingDate = readingDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}

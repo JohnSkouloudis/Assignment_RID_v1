53f338548b400ee93ad1da6e19a8845c158ad50f
iskoulou <johnskoul56@gmail.com>
2024-07-15 12:55:14 +0300

    removed unnecessary methods from ReadingRepository with @Query annotation

--- src/main/java/com/rid/v1/repository/ReadingRepository.java
+++ src/main/java/com/rid/v1/repository/ReadingRepository.java
@@ -1,6 +1,7 @@
 package com.rid.v1.repository;
 
 import com.rid.v1.entity.Reading;
+import com.rid.v1.entity.ReadingRecord;
 import com.rid.v1.entity.Sensor;
 import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.data.domain.Page;
@@ -27,35 +28,16 @@ public interface ReadingRepository extends JpaRepository<Reading,Integer> {
     boolean existsBySensor(Sensor sensor);
 
 
-    @Query(value = "SELECT AVG(reading_value) from readings where sensor_id =:sensorId",nativeQuery = true)
-    Double findMeanOfReadingValueBySensorId(@Param("sensorId") int sensorId);
+
 
     @Query(value = "Select sr.description,sr.reading_date,sr.reading_type,sr.reading_value,sr.time from readings sr join sensors s on sr.sensor_id=s.sensor_id "+
                    "WHERE (:type IS NULL OR s.type = :type) " +
                    "AND (:location IS NULL OR s.location = :location) " +
                    "AND (:time IS NULL OR sr.time = :time)",nativeQuery = true)
-    List<Object[]> findSensorReadingBySensor(@Param("type") String type,
-                                                     @Param("location")String location,
-                                                     @Param("time") String time,
-                                                     Pageable pageable);
+    List<ReadingRecord> findSensorReadingBySensor(@Param("type") String type,
+                                                  @Param("location")String location,
+                                                  @Param("time") String time,
+                                                  Pageable pageable);
 
-    @Query(value = "Select sr.reading_value from readings sr join sensors s on sr.sensor_id=s.sensor_id "+
-            "WHERE (:type IS NULL OR s.type = :type) " +
-            "AND (:location IS NULL OR s.location = :location) " +
-            "AND (:time IS NULL OR sr.time = :time)" +
-            "order by sr.reading_value desc limit 10",nativeQuery = true)
-    List<Double> find10MaxReadingValuesBySensor(@Param("type") String type,
-                                                @Param("location")String location,
-                                                @Param("time") String time,
-                                                Pageable pageable);
 
-    @Query(value = "Select sr.reading_value from readings sr join sensors s on sr.sensor_id=s.sensor_id "+
-            "WHERE (:type IS NULL OR s.type = :type) " +
-            "AND (:location IS NULL OR s.location = :location) " +
-            "AND (:time IS NULL OR sr.time = :time)" +
-            "order by sr.reading_value asc limit 10",nativeQuery = true)
-    List<Double> find10MinReadingValuesBySensor(@Param("type") String type,
-                                                @Param("location")String location,
-                                                @Param("time") String time,
-                                                Pageable pageable);
 }

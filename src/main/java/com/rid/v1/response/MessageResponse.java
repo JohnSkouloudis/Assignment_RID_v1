66d21f9733409c0f90e37d4b8f87b61bccc5662f
iskoulou <johnskoul56@gmail.com>
2024-07-18 15:13:18 +0300

    added lombok to MessageResponse

--- src/main/java/com/rid/v1/response/MessageResponse.java
+++ src/main/java/com/rid/v1/response/MessageResponse.java
@@ -1,18 +1,16 @@
 package com.rid.v1.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class MessageResponse {
 
     private String message;
 
-    public MessageResponse(String message) {
-        this.message = message;
-    }
 
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
 }
